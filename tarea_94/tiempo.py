4bad291b6a6d46d430463edbf2801ee67bddde4d
CJACRed <38211440+CJACRed@users.noreply.github.com>
2022-10-18 19:40:38 +0200

    tarea_94

---
+++ tarea_94/tiempo.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def tierra(tiempocaida):
+    gravedad = 9.8
+    altura = 1/2*gravedad*tiempocaida**2
+    return altura
+
+
+# In[ ]:
+
+
+def marte(tiempocaida):
+    gravedad = 3.7
+    altura = 1/2*gravedad*tiempocaida**2
+    return altura
+
+
+# In[ ]:
+
+
+def jupiter(tiempocaida):
+    gravedad = 24.8
+    altura = 1/2*gravedad*tiempocaida**2
+    return altura
+
